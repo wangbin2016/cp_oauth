a2e59e2bd060986e64ac0030b8f98585451cdcb9
wangb <wangb@DESKTOP-F98CAL0>
2018-03-09 16:18:10 +0800

    init

---
+++ src/main/java/com/caipiao/oauth2/CpOauthApplication.java
@@ -0,0 +1,21 @@
+package com.caipiao.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CpOauthApplication extends WebMvcConfigurerAdapter{
+	public static void main(String[] args) {
+		SpringApplication.run(CpOauthApplication.class, args);
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
+	}
+}
---
+++ src/test/java/com/caipiao/oauth2/CpOauthApplicationTests.java
@@ -0,0 +1,12 @@
+package com.caipiao.oauth2;
+
+import org.junit.Test;
+
+
+public class CpOauthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
